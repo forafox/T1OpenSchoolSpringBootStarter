4eab9c152f432972d16a51c7ed705198bf29b446
forafox <deskpa@yandex.ru>
2024-05-31 01:51:13 +0300

    feat: add dependencies, Main.class

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,24 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.3.0"
+    id("io.spring.dependency-management") version "1.1.4"
 }
 
 group = "com.forafox"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
